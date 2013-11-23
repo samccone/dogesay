482038f12671c6d922802a628879379dfd550410
Sam Saccone <samccone@gmail.com>
2013-11-23 00:54:57 -0500

    stream back a rectangle for all paths

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
+var fs = require('fs');
+var Canvas = require('canvas');
+
 exports.set = function(app) {
-  app.get('/', homePage);
+  app.get('*', function(req, res) {
+    res.setHeader("content-type", "image/jpeg");
+
+    canvas = new Canvas(200, 200)
+    ctx = canvas.getContext('2d');
+    ctx.fillText("Awesome!", 0, 0);
+    canvas.jpegStream().pipe(res)
+  });
 }
 
-/*
- * GET home page.
- */
-function homePage(req, res) {
-  res.render('index', { title: 'Express' });
-}
