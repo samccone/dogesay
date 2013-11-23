114047206dc2ef3bef03ecf613b7c23e1ba79e52
Sam Saccone <samccone@gmail.com>
2013-11-23 00:59:07 -0500

    switch to png stream
    since it is not sync

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,24 @@ var Canvas = require('canvas');
 
 exports.set = function(app) {
   app.get('*', function(req, res) {
-    res.setHeader("content-type", "image/jpeg");
+    res.setHeader("content-type", "image/png");
 
     canvas = new Canvas(200, 200)
     ctx = canvas.getContext('2d');
-    ctx.fillText("Awesome!", 0, 0);
-    canvas.jpegStream().pipe(res)
+
+    ctx.font = '30px impact';
+    ctx.rotate(.1);
+    ctx.fillText("awesome!", 50, 100);
+
+    var te = ctx.measureText('awesome!');
+    ctx.strokestyle = 'rgba(0,0,0,0.5)';
+    ctx.beginPath();
+    ctx.lineTo(50, 102);
+    ctx.lineTo(50 + te.width, 102);
+    ctx.stroke();
+
+
+    canvas.pngStream().pipe(res)
   });
 }
 
